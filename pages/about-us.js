efc31df48ce8c8dadcdae93f5692d8f998ea57ff
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-12-13 15:03:34 -0600

    updated the about-us page

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,5 +1,15 @@
 const Page = () => {
-  return <h1>About us Page</h1>;
+  return (
+    <div>
+      <h1>About us Page</h1>
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo
+      </p>
+    </div>
+  );
 };
 
 export default Page;
